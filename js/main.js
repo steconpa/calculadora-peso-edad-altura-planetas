d14e9fc8780a564f741d7931ed258b3f47ae7e05
Stevens Contreras <116411323+steconpa@users.noreply.github.com>
2023-03-13 23:47:38 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,41 +6,144 @@ let weightInput = document.querySelector('#weight');
 let heightInput = document.querySelector('#height');
 let ageInput = document.querySelector('#age');;
 
-const articlePlanet=document.getElementById("flex-articles");
-const templateArticlePlanet=document.getElementById("template-articlePlanet").content;
-const fragment=document.createDocumentFragment();
-const earthGravity=systemSolarPlanets[2].gravedad;
-const earthDaysYear=systemSolarPlanets[2].periodoOrbitalDias;
-const formUserValues=document.querySelector("#formUserValues");
-let userWeight, userAge;
 
 // Escuchar el evento submit del formulario
 form.addEventListener("submit", function (event) {
   // Prevenir el comportamiento por defecto del formulario
   event.preventDefault();
-  userWeight=event.target.inputWeight.value;
-  userAge=event.target.inputAge.value;
-  event.target.inputWeight.value='';
-  event.target.inputAge.value='';
-  calcNewValues(userWeight,userAge,systemSolarPlanets);
-})
+  
+  // Crear una nueva instancia de la clase FormValidator
+  const formValidator = new FormValidator();
+  const validateResult = formValidator.validateFormData(weightInput, heightInput, ageInput);
 
-function calcNewValues(numPeso,numAge,arrayPlanets){
-  let newWeight=0; 
-  let newAge=0;
-  const unidadPeso=" Kg.";
-  const unidadEdad=" años";
-  arrayPlanets.forEach(element =>{
-    newWeight=Number((numPeso*element.gravedad/earthGravity).toFixed(2));
-    newAge=Number((numAge*earthDaysYear/element.periodoOrbitalDias).toFixed(2));
-    templateArticlePlanet.querySelector('.pPeso').textContent=newWeight+unidadPeso;
-    templateArticlePlanet.querySelector('.pEdad').textContent=newAge+unidadEdad;
-    templateArticlePlanet.querySelector('img').setAttribute('src',element.imgPlanet);
-    templateArticlePlanet.querySelector('img').setAttribute('alt',"Planeta "+element.planeta);
-    templateArticlePlanet.querySelector('h2 a').textContent=element.planeta;
-    templateArticlePlanet.querySelector('h2 a').setAttribute('name',"planeta"+element.planeta);
-    const clone=templateArticlePlanet.cloneNode(true);
-    fragment.appendChild(clone);
+  //Valida los datos ingresados son validos
+  if(validateResult){
+    // Eliminar los resultados anteriores
+    const section = document.querySelector("#planets-results");
+    section.innerHTML = '';
+
+    //Ejecuta la función para calcular 
+    calculateResults( weightInput.value, heightInput.value, ageInput.value );
+
+    form.reset();
+  }
+
+});
+
+class FormValidator {
+  // Valores mínimos y máximos para cada entrada
+  inputRanges = {
+    age: [1, 211], // Rango de edades válidas
+    weight: [5, 691], // Rango de pesos válidos
+    height: [43, 397], // Rango de alturas válidas
+  };
+
+  // Mensajes de error para cada entrada
+  errorMessages = {
+    age: "Ingrese una edad entre 1 y 211 años",
+    weight: "Ingrese un peso entre 5 y 691 Kg.",
+    height: "Ingrese una altura entre 43 y 397 cm.",
+  };
+
+  // Método para validar una entrada
+  validateInput(input, inputType) {
+    // Obtener los valores mínimos y máximos y el mensaje de error correspondiente a la entrada
+    const [min, max] = this.inputRanges[inputType];
+    const errorMessage = this.errorMessages[inputType];
+
+    // Validar la entrada
+    if (!input.value || input.value < min || input.value > max) {
+      // Marcar la entrada como inválida y mostrar el mensaje de error
+      input.classList.add("invalid-input");
+      input.nextElementSibling.innerText = errorMessage;
+      return false;
+    } else {
+      // Marcar la entrada como válida y eliminar cualquier mensaje de error previo
+      input.classList.remove("invalid-input");
+      input.nextElementSibling.innerText = "";
+      return true;
+    }
+  }
+
+  // Método para validar el formulario completo
+  validateFormData(weightInput, heightInput, ageInput) {
+    // Validar cada entrada utilizando el método validateInput de esta misma clase
+    const isValidAge = this.validateInput(ageInput, "age");
+    const isValidWeight = this.validateInput(weightInput, "weight");
+    const isValidHeight = this.validateInput(heightInput, "height");
+
+    // Devolver verdadero solo si todas las entradas son válidas
+    return isValidAge && isValidWeight && isValidHeight;
+  }
+}
+
+//Función para calcular el peso, altura y edad en cada planeta
+function calculateResults( weightInput, heightInput, ageInput ){
+  
+  // Crear un fragmento de documento para contener el contenido generado
+  const fragment = new DocumentFragment();
+    
+  // Itera sobre cada planeta en el array "planetsData"
+  planetsData.forEach((planet) => {
+      
+    // Calcular el peso y la edad en cada planeta
+    const planetWeight = calculateWeight(weightInput, planet.gravity);
+    const planetHeight = calculateHeight(heightInput, planet.gravity);
+    const planetAge = calculateAge(ageInput, planet.yearLength);
+      
+    // Crear una copia del template del planeta y asignar los valores correspondientes
+    const planetTemplate = document.getElementById("planet-template");
+    const planetCopy = planetTemplate.content.cloneNode(true);
+    
+    // Actualiza los valores de los elementos en la copia del template
+    const planetArticle = planetCopy.querySelector(".planet-result");
+    planetArticle.id= planet.name;
+    
+    const planetName = planetCopy.querySelector(".planet-presentation h2");
+    planetName.textContent = planet.name;
+      
+    const planetImage = planetCopy.querySelector("img");
+    planetImage.src = planet.image;
+    planetImage.alt = `Imagen de ${planet.name}`;
+      
+    const planetDescription = planetCopy.querySelector(".planet-presentation p");
+    planetDescription.textContent = planet.description;
+      
+    const planetWeightSpan = planetCopy.querySelector(".planet-weight p span");
+    planetWeightSpan.textContent = planetWeight.toFixed(2);
+      
+    const planetHeightSpan = planetCopy.querySelector(".planet-height p span");
+    planetHeightSpan.textContent = planetHeight.toFixed(2);
+
+    const planetAgeSpan = planetCopy.querySelector(".planet-age p span");
+    planetAgeSpan.textContent = planetAge;
+      
+    const planetRadius = planetCopy.querySelector("li:nth-of-type(1) span");
+    planetRadius.textContent = planet.radius;
+      
+    const planetMass = planetCopy.querySelector("li:nth-of-type(2) span");
+    planetMass.textContent = planet.mass;
+      
+    const planetGravity = planetCopy.querySelector("li:nth-of-type(3) span");
+    planetGravity.textContent = planet.gravity;
+      
+    const planetDay = planetCopy.querySelector("li:nth-of-type(4) span");
+    planetDay.textContent = planet.dayLength;
+      
+    const planetYear = planetCopy.querySelector("li:nth-of-type(5) span");
+    planetYear.textContent = planet.yearLength;
+      
+    const planetDistance = planetCopy.querySelector("li:nth-of-type(6) span");
+    planetDistance.textContent = planet.distanceFromSun;
+      
+    const planetTemperature = planetCopy.querySelector("li:nth-of-type(7) span");
+    planetTemperature.textContent = planet.surfaceTemperature;
+
+    const planetMoreInfo = planetCopy.querySelector("li:nth-of-type(8) a");
+    planetMoreInfo.href = planet.moreInfo;
+      
+    // Agregar la información del planeta al fragmento de documento
+    fragment.appendChild(planetCopy);
   });
     
   // Agregar el contenido generado al documento
